daab53fec53995eb5bebe567462074ef6f36ea57
Steven <stevencjkuo@gmail.com>
2025-12-30 15:00:23 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/'; // 注意這裡要加斜線，對應你的 importmap
+import App from './App.tsx';      // 瀏覽器環境下建議補上副檔名
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
